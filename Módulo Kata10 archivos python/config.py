10138f853d67be0dafdc2ea67b2938f524b97045
GaiskaSalomon <stellardevgeek@gmail.com>
2022-02-14 16:36:07 -0600

    add: Katas curso introductorio

---
+++ Módulo Kata10 archivos python/config.py
@@ -0,0 +1,40 @@
+#'''
+def main():
+    try:
+       configuration = open('config.txt')
+    except FileNotFoundError:
+       print("Couldn't find the config.txt file!")
+#'''
+
+'''
+def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")
+'''
+
+'''
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+'''
+
+'''
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+'''
+
+if __name__ == '__main__':
+    main()
